475d48442791c78b184a5de6a56256f236fe2184
Enpassant <kalman.ferenc@akribis.hu>
2019-08-05 14:53:07 +0200

    Add use function to Stream with automatic closing

--- src/main/java/fp/util/StreamUtil.java
+++ src/main/java/fp/util/StreamUtil.java
@@ -9,12 +9,25 @@ public final class StreamUtil {
         R init,
         BiFunction<R, T, R> fn
     ) {
-        return items -> items
-            .reduce(
+        return items -> {
+        	R result = items.reduce(
                 init,
                 fn,
                 (u1, u2) -> u1
             );
+        	items.close();
+        	return result;
+        };
     }
+
+    public static <T, R> Function<Stream<T>, R> use(
+            Function<Stream<T>, R> fn
+        ) {
+            return items -> {
+            	R result = fn.apply(items);
+            	items.close();
+            	return result;
+            };
+        }
 }
 
