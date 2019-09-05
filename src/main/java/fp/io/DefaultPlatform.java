470372f3bb763ccdc796f7116e4a3740f3df67d3
Enpassant <kalman.ferenc@akribis.hu>
2019-09-05 11:03:21 +0200

    Add own pool names

--- src/main/java/fp/io/DefaultPlatform.java
+++ src/main/java/fp/io/DefaultPlatform.java
@@ -3,13 +3,23 @@ package fp.io;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ThreadFactory;
 
 public class DefaultPlatform implements Platform {
-	private final ExecutorService blocking = Executors.newCachedThreadPool();
-	private final ExecutorService executor = Executors.newFixedThreadPool(
-		java.lang.Runtime.getRuntime().availableProcessors()
+	private static int platformCount = 0;
+	
+	private final ExecutorService blocking = Executors.newCachedThreadPool(
+		new PlatformThreadFactory("io-blocking")
 	);
-	private final ExecutorService forkJoin = new ForkJoinPool(2);
+	private final ExecutorService executor = Executors.newFixedThreadPool(
+		java.lang.Runtime.getRuntime().availableProcessors(),
+		new PlatformThreadFactory("io-executor")
+	);
+	private final ExecutorService forkJoin = new ForkJoinPool(1);
+	
+	public DefaultPlatform() {
+		platformCount++;
+	}
 	
 	@Override
 	public void shutdown() {
@@ -31,5 +41,19 @@ public class DefaultPlatform implements Platform {
 	@Override
 	public ExecutorService getForkJoin() {
 		return forkJoin;
+	}
+
+	class PlatformThreadFactory implements ThreadFactory {
+		private final String poolName;
+		private int threadCount = 0;
+		
+		public PlatformThreadFactory(String poolName) {
+			this.poolName = poolName;
+		}
+
+		public Thread newThread(Runnable r) {
+			threadCount++;
+			return new Thread(r, poolName + "-" + platformCount + "-thread-" + threadCount);
+		}
 	}
 }
