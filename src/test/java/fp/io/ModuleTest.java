b3749cc77924c445ff1738363eccf656051ddea8
Enpassant <kalman.ferenc@akribis.hu>
2019-09-06 16:37:13 +0200

    Add Console module

---
+++ src/test/java/fp/io/ModuleTest.java
@@ -0,0 +1,59 @@
+package fp.io;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Test;
+
+import fp.util.Failure;
+import fp.util.Right;
+
+public class ModuleTest {
+	final static DefaultPlatform platform = new DefaultPlatform();
+	
+	final Runtime<Void> defaultVoidRuntime = new DefaultRuntime<Void>(null, platform);
+	
+	@AfterClass
+    public static void setUp() {
+		platform.shutdown();
+    }
+	
+	private class TestConsole implements Console.Service {
+    	private final StringBuilder sb = new StringBuilder();
+    	private final String[] inputs;
+    	private int inputIndex = 0;
+    	
+    	public TestConsole(final String... inputs) {
+    		this.inputs = inputs;
+		}
+    	
+		public IO<Object, Object, Void> println(String line) {
+			return IO.effectTotal(() -> { sb.append(line).append("\n"); });
+		}
+		public IO<Object, Failure, String> readLine() {
+			return IO.effect(() -> inputs[inputIndex++]);
+		}
+		public String getOutputs() {
+			return sb.toString();
+		}
+	}
+	
+    @Test
+    public void testConsole() {
+        IO<Console.Service, Object, String> io =
+        	Console.println("Good morning, what is your name?").flatMap(
+    		v -> Console.readLine().flatMap(
+    		name -> Console.println("Good to meet you, " + name + "!").map(
+    		v2 -> name)
+    	));
+        
+        final TestConsole testConsole = new TestConsole("John");
+		Assert.assertEquals(
+        	Right.of("John"),
+        	defaultVoidRuntime.unsafeRun(io.provide(testConsole))
+        );
+		Assert.assertEquals(
+			"Good morning, what is your name?\n" + "Good to meet you, John!\n",
+        	testConsole.getOutputs()
+        );
+    }
+}
